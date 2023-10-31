5dd7761a3adf4b8843c4faa862af93f33053718d
Ramon William <roniewill@gmail.com>
2023-10-31 00:42:47 -0300

    add some tweaks on user sessions

--- db/seeds/initial_seed.rb
+++ db/seeds/initial_seed.rb
@@ -2,13 +2,13 @@
 
 # USERS
 users = [
-  { first_name: 'Main User', last_name: 'First', email: 'user@email.com', password: 'pass12' },
-  { first_name: 'Second User', last_name: 'Secondary', email: 'second@email.com', password: 'pass12' }
+  { first_name: 'Major User', last_name: 'First', email: 'admin@email.com', password: 'pass12', role: 2 },
+  { first_name: 'Second User', last_name: 'Secondary', email: 'second@email.com', password: 'pass12', role: 0 }
 ]
 
 User.destroy_all
 
 users.each do |user|
   puts "Creating user #{user[:first_name]}"
-  User.create!(first_name: user[:first_name], last_name: user[:last_name], email: user[:email], password: user[:password], password_confirmation: user[:password])
+  User.create!(first_name: user[:first_name], last_name: user[:last_name], email: user[:email], password: user[:password], password_confirmation: user[:password], role: user[:role])
 end
