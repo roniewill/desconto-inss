b2bb4b45fdae30ec426726a3b43368d4469452bf
Ramon William <roniewill@gmail.com>
2023-10-31 00:24:32 -0300

    add controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'show/:id', to: 'home#show', as: :user_info
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
